703d77ada5bd309e09b86dfc9f473654c887ea0c
salim.aydgn <salim.aydgn@gmail.com>
2024-03-13 04:06:56 +0300

    13-03-2024 04:06

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Api Demo"),
+        title: const Text("API Demo"),
         centerTitle: true,
       ),
       body: Center(
